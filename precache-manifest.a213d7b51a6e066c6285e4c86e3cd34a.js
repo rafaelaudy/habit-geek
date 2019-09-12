7297440479b3384582d40eefb6d922bd03d378e0
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-09-12 15:38:46 +0100

    Updates

---
+++ precache-manifest.a213d7b51a6e066c6285e4c86e3cd34a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f422c7bfd3bf085abbaacbdfef3367",
+    "url": "/habit-geek/index.html"
+  },
+  {
+    "revision": "47267566e09bf56ee8d8",
+    "url": "/habit-geek/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "384f2335442542493a71",
+    "url": "/habit-geek/static/css/main.05494800.chunk.css"
+  },
+  {
+    "revision": "47267566e09bf56ee8d8",
+    "url": "/habit-geek/static/js/2.b940b8ef.chunk.js"
+  },
+  {
+    "revision": "384f2335442542493a71",
+    "url": "/habit-geek/static/js/main.37c0fa8e.chunk.js"
+  },
+  {
+    "revision": "8d3a7da2497ec1145738",
+    "url": "/habit-geek/static/js/runtime~main.9224e52c.js"
+  },
+  {
+    "revision": "fad4fb81428b3b98e0bc8d0574ca11d1",
+    "url": "/habit-geek/static/media/001-burglar.fad4fb81.svg"
+  },
+  {
+    "revision": "972480c336c77593e6de12b32684e2ba",
+    "url": "/habit-geek/static/media/002-woman.972480c3.svg"
+  },
+  {
+    "revision": "30672cbf35290424076dde9f7916b98f",
+    "url": "/habit-geek/static/media/003-superhero.30672cbf.svg"
+  },
+  {
+    "revision": "4a1aa59e7c6aeb0629c8da2357df6889",
+    "url": "/habit-geek/static/media/004-robot.4a1aa59e.svg"
+  },
+  {
+    "revision": "ffaf3a2435bd5963ab09c9044cc56036",
+    "url": "/habit-geek/static/media/005-dragon.ffaf3a24.svg"
+  },
+  {
+    "revision": "605d5cd3ba645847ff6e5a661f60fbc5",
+    "url": "/habit-geek/static/media/006-cyborg.605d5cd3.svg"
+  }
+]);
