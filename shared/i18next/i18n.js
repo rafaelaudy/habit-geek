9b2281a19de7bc7f8eed72ca9aa58a806c398407
marianabergmann <marii.bergmann@hotmail.com>
2019-10-22 18:50:23 +0100

    Updated texts in EN and PT

--- shared/i18next/i18n.js
+++ shared/i18next/i18n.js
@@ -9,14 +9,14 @@ const resources = {
       "nav-history": "History",
       "nav-profile": "Profile",
 
-      "my-habits-title": "What have you done this week{{username}}?",
-      "my-habits-new-habit": "I'll add a new habit!",
+      "my-habits-title": "How is the current week going,{{username}}?",
+      "my-habits-new-habit": "Add new habit!",
 
-      "save-habit-title": "Nice, what will it be?",
+      "save-habit-title": "New habit",
       "save-habit-name": "Name:",
-      "save-habit-name-validation": 'Hey! Trying to commit to "Blank"?',
+      "save-habit-name-validation": 'Eerg... we kind of need this one, right?',
       "save-habit-type": "Type:",
-      "save-habit-type-validation": "Common... Help me help you!",
+      "save-habit-type-validation": "Dude, this field is mandatory.",
       "save-habit-type-options": [
         { key: "Health", label: "Health" },
         { key: "Social", label: "Social" },
@@ -26,19 +26,19 @@ const resources = {
       "save-habit-frequency": "Frequency:",
       "save-habit-frequency-validation": "Funny, that's an easy way to ace it!",
       "save-habit-save": "Let's start!",
-      "save-habit-delete": "Let's scrap this!",
-      "save-habit-cancel": "Maybe tomorrow...",
+      "save-habit-delete": "Delete this!",
+      "save-habit-cancel": "Hmm... Changed my mind!",
 
-      "history-title": "Here is your progress so far:",
+      "history-title": "My past weeks:",
       "history-title-empty-data": "You just started, come back next week!",
 
       "profile-title": "Super hero who?",
       "profile-name": "Name:",
       "profile-name-validation":
-        "What an honor! The invisible habit geek among us?",
+        "What an honor! The non-named habit geek among us?",
       "profile-avatar": "Avatar:",
       "profile-avatar-alt-desc": "Avatar, not really important. Don't worry!",
-      "profile-save": "Done!",
+      "profile-save": "Save!",
 
       "date-days-short": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
       "date-months-short": [
@@ -60,18 +60,18 @@ const resources = {
 
   pt: {
     translation: {
-      "nav-my-habits": "Meus hábitos",
+      "nav-my-habits": "Meus Hábitos",
       "nav-history": "Histórico",
       "nav-profile": "Perfil",
 
-      "my-habits-title": "O que você fez nessa semana{{username}}?",
-      "my-habits-new-habit": "Bora novo hábito!",
+      "my-habits-title": "Como a semana atual está indo,{{username}}?",
+      "my-habits-new-habit": "Criar novo hábito!",
 
-      "save-habit-title": "Legal, qual vai ser?",
+      "save-habit-title": "Novo hábito",
       "save-habit-name": "Nome:",
-      "save-habit-name-validation": "Ei! E o nome?",
+      "save-habit-name-validation": "Umm... esse campo meio que é importante, não?",
       "save-habit-type": "Tipo:",
-      "save-habit-type-validation": "Ele é importante porque?",
+      "save-habit-type-validation": "Campo obrigatório.",
       "save-habit-type-options": [
         { key: "Health", label: "Saúde" },
         { key: "Social", label: "Social" },
@@ -80,10 +80,10 @@ const resources = {
       ],
       "save-habit-frequency": "Frequência:",
       "save-habit-frequency-validation":
-        "Hábito sem frequência? Aí fica fácil!",
+        "Hábito sem frequência? Aí fica fácil, né?",
       "save-habit-save": "Bora!",
-      "save-habit-delete": "Não agrega mais!",
-      "save-habit-cancel": "Ops, quero voltar...",
+      "save-habit-delete": "Pode apagar!",
+      "save-habit-cancel": "Peraí! Quero voltar!",
 
       "history-title": "Esse é o seu progresso até agora:",
       "history-title-empty-data": "Aparece aqui na semana que vem!",
@@ -91,11 +91,11 @@ const resources = {
       "profile-title": "Quem é você na fila do pão?",
       "profile-name": "Nome:",
       "profile-name-validation":
-        "Que honra! Um habit geeker invisível no meio de nós?",
+        "Que honra! Um habit geeker que-não-pode-ser-nomeado no meio de nós?",
       "profile-avatar": "Avatar:",
       "profile-avatar-alt-desc":
         "Avatar, não é muito importante. Não se preocupe!",
-      "profile-save": "Feito!",
+      "profile-save": "Salvar!",
 
       "date-days-short": ["Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"],
       "date-months-short": [
